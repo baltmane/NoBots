17b50cee7a5b00b9a872c99b493cf200f6b802b6
baltmane <32001157+baltmane@users.noreply.github.com>
2018-04-30 14:24:26 -0700

    Add files via upload

---
+++ html/db.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","db01","bVbo9rbi8VGmnWL3qzzR","register");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
